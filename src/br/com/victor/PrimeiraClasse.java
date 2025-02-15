ff74ee4a7c29ace62d00bb4fa6dbaddc8a3aacd0
VictorLR <victoraffonso1997@gmail.com>
2025-02-15 20:04:06 -0300

    primeiro commit do exercicio

---
+++ src/br/com/victor/PrimeiraClasse.java
@@ -0,0 +1,8 @@
+package br.com.victor;
+
+public class PrimeiraClasse {
+    public static void main(String[] args) {
+        String nome = "Victor";
+        System.out.print("Olá " + nome);
+    }
+}
